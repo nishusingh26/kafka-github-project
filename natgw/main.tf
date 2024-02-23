55e8045ea2fd7323c7eab4e835907d18f771cc3b
Nishu Singh <79636190+nishusingh26@users.noreply.github.com>
2024-02-23 17:02:39 +0530

    Update main.tf

--- natgw/main.tf
+++ natgw/main.tf
@@ -4,7 +4,7 @@ resource "aws_eip" "kafka-nat" {
 
 resource "aws_nat_gateway" "kafka-nat-gw" {
   allocation_id = aws_eip.kafka-nat.id
-  subnet_id     = "subnet-0123be5300755d985"
+  subnet_id     = "subnet-0b3649cc8734b995d"
 
   tags = {
     Name = var.nat_tag
