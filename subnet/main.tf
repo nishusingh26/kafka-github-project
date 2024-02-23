35f746aeea01d4032ecd51e894ad0cf0586cddb9
Nishu Singh <79636190+nishusingh26@users.noreply.github.com>
2024-02-23 16:24:09 +0530

    Update main.tf

--- subnet/main.tf
+++ subnet/main.tf
@@ -5,7 +5,7 @@ resource "aws_subnet" "Private_Subnet" {
   count = length(var.private-subnet)
   //az_count = "${length(data.aws_availability_zones.available.names)}"
 
-  vpc_id     = "vpc-0641b491c4e5aa89a"
+  vpc_id     = "vpc-00e59ad4b212fe57b"
   cidr_block = var.private-subnet[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
